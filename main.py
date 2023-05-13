7cba4cf6c309c237819cbfb42efb6fc2b1597cae
JakubK <j.kostasky@gmail.com>
2023-05-13 14:26:51 +0200

    hi there, Jakub here!

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask('app')
 def index():
     return 'Hello World!'
 
+@app.route('/2')
+def index2():
+    return 'Hi there, Jakub here!'
+
 @app.route('/ret', methods=['POST'])
 def ret():
     json = request.json
