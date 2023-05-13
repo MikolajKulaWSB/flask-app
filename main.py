d2b57adbc36c01947d5cd3a4ef76180357f87c3a
Mikolaj Kula <alterius@kali.alterius.com>
2023-05-13 13:48:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask('app')
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/ret', methods=['POST'])
+def ret():
+    json = request.json
+    x = json['x']
+    print(x)
+    return jsonify({'x': x})
+
+if __name__ == '__main__':
+    app.run()
